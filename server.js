d0ba708f112e4eccfcb78f37e3dd4077eb5c42bf
Henrique Junqueira <mhbj@pop-os.localdomain>
2020-09-17 14:29:24 -0300

    database connections

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+     { useNewUrlParser: true}
+);
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send('Hello, world');
 });
